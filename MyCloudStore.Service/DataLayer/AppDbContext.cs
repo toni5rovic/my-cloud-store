9d3dc427b30bf2a8d6e9759067f47e6a7c76acc9
toni5rovic <toni.petrovic@elfak.rs>
2019-12-19 21:28:06 +0100

    data: add db context

---
+++ MyCloudStore.Service/DataLayer/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyCloudStore.Service.DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCloudStore.Service.DataLayer
+{
+    public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>
+    {
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<File> Files { get; set; }
+
+        public AppDbContext() 
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
