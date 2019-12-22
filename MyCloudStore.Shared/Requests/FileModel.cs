d96b6d049b48b0bd46a4df987ba8ff3307b1b87e
toni5rovic <toni.petrovic@elfak.rs>
2019-12-23 00:35:58 +0100

    shared: remove previous models, add file model

---
+++ MyCloudStore.Shared/Requests/FileModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCloudStore.Shared.Requests
+{
+	public class FileModel
+	{
+		public string FileName { get; set; }
+		public IFormFile Content { get; set; }
+		public string HashValue { get; set; }
+	}
+}
